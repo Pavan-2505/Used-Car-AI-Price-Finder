7acc1dd353edde67d9810e69f1f4984bf8e68b13
Pavan-2505 <pv25beast@gmail.com>
2026-09-13 23:43:40 +0530

    Add full column display for dataset inspection

--- UsedCar1.py
+++ UsedCar1.py
@@ -3,5 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
+
 warnings.filterwarnings('ignore')
-df1=pd.read_csv('train.csv')
+
+df1 = pd.read_csv('train.csv')
+
+print("The Dataset contains", df1.shape[0], "rows and", df1.shape[1], "columns")
+
+print(df1.info())
+
+pd.set_option('display.max_columns', None)
+
+print(df1.head())
