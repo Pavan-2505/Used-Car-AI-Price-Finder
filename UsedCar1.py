05df6f4c7c2f1e0886d2a45feac3580ee4e6690a
Pavan-2505 <pv25beast@gmail.com>
2026-09-14 09:17:21 +0530

    Add initial dataset inspection

--- UsedCar1.py
+++ UsedCar1.py
@@ -6,12 +6,21 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
+#Load Datasets
 df1 = pd.read_csv('train.csv')
-
-print("The Dataset contains", df1.shape[0], "rows and", df1.shape[1], "columns")
-
-print(df1.info())
+df2=pd.read_csv('Car Sell Dataset.csv')
 
 pd.set_option('display.max_columns', None)
 
+#Dataset 1
+print("---------FIRST DATASET---------")
+print("The Dataset contains", df1.shape[0], "rows and", df1.shape[1], "columns")
+df1.info()
 print(df1.head())
+
+#Dataset 2
+print("---------SECOND DATASET---------")
+print(f"The Dataset contains {df2.shape[0]} rows and {df2.shape[1]} columns")
+df2.info()
+print(df2.head())
+
